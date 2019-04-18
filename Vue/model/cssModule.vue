f179b70c4135c72a248478458039f97bef0c4a2e
json <ooo88888@live.cn>
2019-04-18 23:54:38 +0800

    多个筛选

---
+++ Vue/model/cssModule.vue
@@ -0,0 +1,23 @@
+<template>
+    <button :class="[$style.button, $style.buttonClose]">X</button>
+</template>
+
+<script>
+	export default {
+		name: 'cssModule',
+	};
+</script>
+<!-- 使用 CSS Modules -->
+<style module>
+    .button {
+        border: none;
+        border-radius: 2px;
+    }
+
+    .buttonClose {
+        background-color: red;
+    }
+</style>
+
+
+
