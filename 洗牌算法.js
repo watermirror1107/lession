173d8ee42eaf81117ff5b28dca8b6865a6882019
songjie55 <ooo88888@live.cn>
2020-05-06 14:40:18 +0800

    洗牌算法

---
+++ 洗牌算法.js
@@ -0,0 +1,26 @@
+//洗牌算法
+//原地洗牌
+function shuffle(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let randomNum = i + Math.round(Math.random() * (arr.length - 1 - i));
+        console.log(randomNum);
+        arr[i] = [arr[randomNum], arr[randomNum] = arr[i]][0];
+    }
+    return arr;
+}
+
+//非原地洗牌
+function shuffle2(arr) {
+    let _arr = [];
+    while (arr.length) {
+        let randomNum = Math.floor(arr.length * Math.random());
+        _arr.push(arr.splice(randomNum, 1)[0]);
+    }
+    return _arr;
+}
+
+let testArr1 = [1, 2, 3, 4, 5, 6, 7];
+let testArr2 = [1, 2, 3, 4, 5, 6, 7];
+console.log(shuffle(testArr1));
+console.log(shuffle2(testArr2));
+
