e7e2f229a3347370ca2be16abf0f26e8b2f5399d
songjie55 <ooo88888@live.cn>
2020-05-12 18:13:57 +0800

    添加数据监听

---
+++ 手写vue数据劫持深度监听/defineproperty.js
@@ -0,0 +1,18 @@
+function update() {
+    console.log('触发了');
+}
+
+function observeData(data) {
+    Object.keys(data).map((key) => {
+
+    });
+}
+
+function setProperty(key) {
+
+}
+
+let testData = {name: 'ka', age: 19, family: {father: 'dk', mother: 'jk'}};
+
+
+
