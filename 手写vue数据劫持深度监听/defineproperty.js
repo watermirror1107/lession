10a12c27317774a8b5f4f5b3f6d291441bf595ef
songjie55 <ooo88888@live.cn>
2020-05-17 23:18:48 +0800

    深度监听数据，v1。2添加数组监听

--- 手写vue数据劫持深度监听/defineproperty.js
+++ 手写vue数据劫持深度监听/defineproperty.js
@@ -1,6 +1,17 @@
+//defineproperty缺点，1深度监听需要递归，一次计算量大，2无法监听删除和添加属性,3对数组需要做特殊处理
 function update() {
     console.log('触发了');
 }
+
+//定义一个新的数组原型，拥有原数组原型的方法并且数据可以被监听,又不会对原数组的方法造成影响
+const vArrProperty = Object.create(Array.prototype);
+['push', 'pop', 'unshift', 'splice', 'shift'].forEach(methodName => {
+    vArrProperty[methodName] = function () {
+        update();
+        Array.prototype[methodName].call(this, ...arguments);
+    };
+});
+
 
 function observeData(data) {
     if (typeof data != 'object' || data == null) {
@@ -10,6 +21,9 @@ function observeData(data) {
         if (typeof data[key] == 'object') {
             //因为defineproperty只能监听第一层的属性，除非family属性改变才会被触发，所以这里要做递归才可以深度监听family对象里面的属性
             observeData(data[key]);
+        }
+        if (data[key] instanceof Array) {
+            data[key].__proto__ = vArrProperty;
         }
         let o = data[key];//这里要复制一下值，不然堆栈溢出
         Object.defineProperty(data, key, {
@@ -27,6 +41,6 @@ function observeData(data) {
     }
 }
 
-let dd = {name: 'ka', age: 19, family: {father: 'dk', mother: 'jk'}};
+let dd = {name: 'ka', age: 19, family: {father: 'dk', mother: 'jk'}, arr: [1, 2, 3]};
 observeData(dd);
 
