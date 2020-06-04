3fac750fac6693d0131b85c7cd59b0535931d0bc
songjie55 <ooo88888@live.cn>
2020-06-04 17:19:01 +0800

    proxy代理数据方法

--- 手写vue数据劫持深度监听/proxy.js
+++ 手写vue数据劫持深度监听/proxy.js
@@ -1,1 +1,16 @@
+function proxyData(obj) {
+    return new Proxy(obj, {
+        set(target, p, value, receiver) {
+            // console.log(target);//目标对象
+            // console.log(p);//属性
+            // console.log(value);//新的值
+            // console.log(receiver);//得到代理后的实例
+        },
+        get(target, p, receiver) {
+            return target[p];
+        }
+    });
+}
 
+let testData = {name: 'asdf', age: 18};
+let obj = proxyData(testData);
