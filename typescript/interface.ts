7530c29b8d786ef89eca60d3490494d017ea6ee7
songjie55 <ooo88888@live.cn>
2020-07-13 18:10:57 +0800

    typescript

--- typescript/interface.ts
+++ typescript/interface.ts
@@ -1,10 +1,13 @@
 interface interface1 {
-    label: string;//传入的参数必须带有这个属性
+    label: string;//传入的参数必须带有这个属性并且类型是字符串
+    size?: number//非必要参数，但是传的时候必须是数字类型
 }
 
 function test(obj: interface1) {
     console.log(obj.label);
 }
 
-let myObj = {size: 10, label: "Size 10 Object"};
-test(myObj);
+let obj1 = {size: 'asdfdf', label: "Size 10 Object"};
+let obj2 = {size: 111, label: "Size 10 Object"};
+test(obj1);
+test(obj2)
