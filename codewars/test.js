8d4300a5674461b8fb8bcb578999dc7cbe7b138e
songjie55 <ooo88888@live.cn>
2020-07-21 14:00:00 +0800

    ts类型和接口

--- codewars/test.js
+++ codewars/test.js
@@ -520,5 +520,24 @@ snail = function (arr) {
     return resArr;
 };
 
-snail([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);//[1, 2, 3, 6, 9, 8, 7, 4, 5]
+// snail([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);//[1, 2, 3, 6, 9, 8, 7, 4, 5]
 // snail([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20], [21, 22, 23, 24, 25]]);//[1, 2, 3, 4, 5, 10, 15, 20, 25, 24, 23, 22, 21, 16, 11, 6, 7, 8, 9, 14, 19, 18, 17, 12, 13]
+let str = 'user.name.firstname=Bob&user.name.lastname=Smith&user.favoritecolor.dd=Light%20Blue';
+
+function convertQueryToMap(query) {
+    var obj = {};
+    query.split('&').map(function (params) {
+        var parts = params.split('=');
+        if (!parts[1]) return {};
+        parts[0].split('.').reduce(function (cur, next, i, arr) {
+            if (!cur[next]) cur[next] = {};
+            if (i === arr.length - 1) cur[next] = decodeURIComponent(parts[1]);
+            return cur[next];
+        }, obj);
+    });
+    return obj;
+}
+
+
+convertQueryToMap(str);
+
