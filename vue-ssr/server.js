4473522257ccf00793bf7a44b17222b88c402943
songjie55 <ooo88888@live.cn>
2020-07-23 10:05:37 +0800

    模板渲染

--- vue-ssr/server.js
+++ vue-ssr/server.js
@@ -1,8 +1,24 @@
-let http = require('http');
-let fs = require('fs');
-const server = http.createServer((req, res) => {
-    // 这里可以设置header信息, 如跨域等信息
-    res.writeHead(200, {'Content-Type': 'text/html'});//text/plain会返回文本
-    // 发送响应数据
-    res.end(fs.readFileSync('./template.html', 'utf-8'));
+const http = require('http');
+const fs = require('fs');
+const renderer = require('vue-server-renderer');
+const Vue = require('vue');
+http.createServer((req, res) => {
+    const app = new Vue({
+        template: `<div>123</div>`//这里的模板会插入到模板页面的注释地方<!--vue-ssr-outlet-->
+    });
+    renderer.createRenderer({
+        template: fs.readFileSync('./template.html', 'utf-8')
+    }).renderToString(app, {
+        meta: `<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width">`, title: `测试`
+    }, (err, html) => {
+        if (err) {
+            console.log(err);
+            res.end('Internal Server Error');
+            return;
+        }
+        res.end(html);
+    });
+
 }).listen(8080);
+console.log('started');
