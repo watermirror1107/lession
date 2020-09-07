8a4831a81359230bea97078779d4f6bc027f0573
songjie55 <ooo88888@live.cn>
2020-09-07 23:33:11 +0800

    render生成页面结构，scrollTop是一个盒子共用的

--- Vue/render.js
+++ Vue/render.js
@@ -7,6 +7,6 @@ const template3 = compiler.compile("<p>{{bol?1:2}}</p>");
 console.log(template3.render);//with(this){return _c('p',[_v(_s(bol?1:2))])}
 const template4 = compiler.compile('<div class="box" id="first"><img :src="item.url" alt=""></div>');
 console.log(template4.render);//with(this){return _c('div',{staticClass:"box",attrs:{"id":"first"}},[_c('img',{attrs:{"src":item.url,"alt":""}})])}
-const template5 = compiler.compile('<img v-for="(item,index) in list" :src="item.url" alt="" @click="selctedItem">');
-console.log(template5.render);//with(this){return _l((list),function(item,index){return _c('img',{attrs:{"src":item.url,"alt":""},on:{"click":selctedItem}})})}
+const template5 = compiler.compile('<img v-for="(item,index) in list" :src="item.url" alt="" @click="selectedItem">');
+console.log(template5.render);//with(this){return _l((list),function(item,index){return _c('img',{attrs:{"src":item.url,"alt":""},on:{"click":selectedItem}})})}
 
---
+++ Vue/testRender.vue
@@ -0,0 +1,28 @@
+<script>
+    export default {
+        name: "testRender",
+        render: function (h) {
+            console.log(this);
+            return h('div', {class: 'test', props: ['testData'], on: {click: 'conData'}}, 'test');
+        },
+        props: ['testData'],
+        methods: {
+            conData() {
+                console.log(122);
+                console.log(this.testData);
+            }
+        },
+        data() {
+            return {
+                age: 18
+            };
+        }
+    };
+</script>
+
+<style scoped>
+    .test {
+        font-size: 18px;
+        color: red;
+    }
+</style>
