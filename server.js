7f2b454b97b23d225ca2c029a226fb9ceb77ec9e
songjie55 <ooo88888@live.cn>
2020-10-12 11:39:38 +0800

    数字雨，替换字符串某一个元素

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 let http=require('http');
 let url=require('url');
-let util=require('util');
 let fs=require('fs');
 let server=http.createServer((req,res)=>{
     let pathName=url.parse(req.url).pathname;
