f8e991a7ecff29b38d4614bd6f6bc3acde7be9bc
songjie55 <ooo88888@live.cn>
2020-12-08 09:39:49 +0800

    添加测试loader的工具

---
+++ vuecomponents/components/oneTree.vue
@@ -0,0 +1,63 @@
+<template>
+    <view class="selectItem">
+        <view class='item' @click="selectItem">
+            <text>{{item.name}}</text>
+            <!-- 添加self解决点击不是箭头也会触发的BUG -->
+            <view @click.stop.self='$emit("switch",$event,item.label)' v-if='item.children&&item.children.length>0'
+                  class='arrow'
+                  :class="item.isRetract?'isClick':''"></view>
+        </view>
+        <div class="children" :style="{height:item.isRetract?'auto':'0'}">
+            <slot></slot>
+        </div>
+    </view>
+</template>
+
+<script>
+    export default {
+        props: {
+            item: null,
+        },
+        inject: ['chooseArea'],//利用父自己提供的依赖改变值
+        methods: {
+            selectItem() {
+                this.chooseArea(this.item)
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .item {
+        width: 100%;
+        box-sizing: border-box;
+        height: 80px;
+        background-color: #fff;
+        padding: 0 40px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        overflow: hidden;
+        transition: all 300ms;
+    }
+
+    .children {
+        padding-left: 20px;
+        overflow: hidden;
+    }
+
+    .arrow {
+        width: 80px;
+        height: 80px;
+        background-image: url(../../static/arrow.png);
+        transform: rotate(-90deg);
+        background-repeat: no-repeat;
+        background-size: 20px 32px;
+        background-position: center center;
+
+    }
+
+    .isClick {
+        transform: rotate(90deg);
+    }
+</style>
---
+++ vuecomponents/components/treeComponents.vue
@@ -0,0 +1,123 @@
+<script>
+    import simgleItem from './simgleItem.vue'
+
+    export default {
+        render(h) {
+            let createChild = (item) => {//递归生成内部树桩结构
+                let res = []
+                if (item.children && item.children.length > 0) {
+                    for (let i = 0; i < item.children.length; i++) {
+                        res.push(h(simgleItem, {
+                            on: {
+                                switch: this.changeRetract
+                            },
+                            props: {
+                                index: i,
+                                item: item.children[i]
+                            },
+                            key: item.children[i].label
+                        }, createChild(item.children[i])))
+                    }
+                }
+                return res
+            }
+            return h('div', {
+                class: 'contain',
+            }, this.list.map((item, index) => {
+                return h(simgleItem, {
+                    on: {
+                        switch: this.changeRetract,
+                    },
+                    props: {
+                        index: index,
+                        item: item
+                    },
+                    key: item.label
+                }, createChild(item))
+            }))
+        },
+        inheritAttrs: false,
+        props: {
+            arr: {
+                type: Array,
+                default: () => [{
+                    name: 'p1',
+                    children: [{
+                        name: 's1'
+                    }]
+                },
+                    {
+                        name: 'p2',
+                        children: [{
+                            name: 's2',
+                            children: [{
+                                name: 'gs1',
+                                children: [{
+                                    name: 'ggs'
+                                },
+                                    {
+                                        name: 'ggs'
+                                    }
+                                ]
+                            }, {
+                                name: 'gs2'
+                            }]
+                        }]
+                    }
+                ]
+            }
+        },
+        components: {
+            simgleItem
+        },
+        data() {
+            return {
+                list: []
+            };
+        },
+        methods: {
+            changeRetract(ev, label) {
+                let arr = String(label).split(':');
+                let x = 0,
+                    obj = null;//对象是个指针，利用x数字和数组长度做判断，递归获取里面的对象
+                while (x <= (arr.length - 1)) {
+                    if (x == 0) {
+                        obj = this.list[arr[x]]
+                    } else {
+                        obj = obj.children[arr[x]]
+                    }
+                    x++;
+                }
+                this.$set(obj, 'isRetract', !obj.isRetract)
+            }
+        },
+        mounted() {
+            let arr = JSON.parse(JSON.stringify(this.arr));
+
+            function addAttr(item, index, parentlabel) {
+                if (parentlabel == null) {
+                    item.label = index
+                } else {
+                    item.label = parentlabel + ':' + index;
+                }
+                if (item.children && item.children.length > 0) {
+                    item.isRetract = false;
+                    for (let i = 0; i < item.children.length; i++) {
+                        addAttr(item.children[i], i, item.label)
+                    }
+                }
+            }
+
+            arr.map((item, index) => {
+                addAttr(item, index, null)
+            });
+            this.list = arr;
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .contain {
+        width: 100%;
+    }
+</style>
