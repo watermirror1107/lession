c93a367dbe284b27f258d7b400a28f073843602b
songjie55 <ooo88888@live.cn>
2021-03-03 18:35:02 +0800

    补充事件流dom3新提供的阻止方法

---
+++ testCode.js
@@ -0,0 +1,27 @@
+let num = 60;
+
+function time() {
+    console.log(num)
+    if (num > 1) {
+        setTimeout(() => {
+            num--;
+            time()
+        }, 1000)
+    }
+}
+
+// time();
+let arr = [1, -3, 8, -7, 5, 3], mid = [];
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+        mid.push(arr[i])
+        arr[i] = 'm'
+    }
+}
+mid.sort()
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 'm') {
+        arr[i] = mid.shift();
+    }
+}
+console.log(arr)
