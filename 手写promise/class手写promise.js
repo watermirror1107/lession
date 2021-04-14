28e5024926fd9a63e6ef015abcb84abfc6d8150b
songjie55 <ooo88888@live.cn>
2021-04-14 22:21:00 +0800

    优化vue promise

--- 手写promise/class手写promise.js
+++ 手写promise/class手写promise.js
@@ -11,6 +11,9 @@ class Promise {
         this.onResolvedCallbacks = [];//一个Promise实例的then方法可以被调用多次，所以这里的then里面的回调需要一个数组来收集
         this.onRejectedCallbacks = [];
         const resolve = (value) => {
+            if (value instanceof Promise) {
+                return value.then(reject, reject)
+            }
             if (this.status === PENDING) {
                 this.value = value;
                 this.status = FULFILLED;
@@ -32,11 +35,16 @@ class Promise {
     }
 
     then(onFulfilled, onRejected) {
+        //防止类型判断，以防传递普通值
+        onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : v => v;
+        onRejected = typeof onRejected === 'function' ? onRejected : err => {
+            throw err
+        };
         let promise2 = new Promise((resolve, reject) => {//这里面的代码立即执行，就相当于new普通一个普通类，里面的函数就当场初始化参数
             //1.订阅发布用于异步
             if (this.status === PENDING) {
                 this.onResolvedCallbacks.push(() => {
-                    //这里用settimeout是为了在规范里面说明onFulfilled和onRejected不能再当前事件循环里执行，只能放在当前事件循环的微任务或者下一个事件循环里,
+                    //这里用setTimeout是为了在规范里面说明onFulfilled和onRejected不能再当前事件循环里执行，只能放在当前事件循环的微任务或者下一个事件循环里,
                     //而且这样才能获取到promise2
                     setTimeout(() => {
                         try {
@@ -88,12 +96,53 @@ class Promise {
         })
         return promise2
     }
+
 }
+
+//减少一次套用 延迟对象
+Promise.deferred = function () {
+    let dfd = {};
+    dfd.promise = new Promise((resolve, reject) => {
+        dfd.resolve = resolve
+        dfd.reject = reject
+    })
+    return dfd
+}
+//一个promise直接返回resolve一个promise
 
 //利用X的值来判断是走promise2的resolve还是reject
 function resolvePromise(promise2, x, resolve, reject) {
-    if (promise2 === x) {
+    if (promise2 === x) {//这两者不能相同
         return reject(new TypeError('错误'))
+    }
+    //要兼容别人的promise
+    if ((typeof x === 'object' && x !== null) || typeof x === 'function') {
+        //promise对象必须有then方法
+        let called = false;//加个开关控制一下promise的状态,这里是为了兼容别人的promise
+        try {
+            //有可能then方法通过defineProperty取值的时候会报错
+            let then = x.then;
+            if (typeof then === 'function') {
+                then.call(x, y => {
+                    if (called) return;
+                    called = true;
+                    resolvePromise(promise2, y, resolve, reject)//这里不是走resolve是因为别人的resolve里面可能又是一个promise对象
+                }, r => {
+                    if (called) return;
+                    called = true;
+                    reject(r)
+                })//指定this
+            } else {
+                resolve(x)
+            }
+        } catch (e) {
+            if (called) return;
+            called = true;
+            reject(e)
+        }
+    } else {
+        //返回普通值直接放到promise2的resolve
+        resolve(x)
     }
 }
 
