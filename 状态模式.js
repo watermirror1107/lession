f1711e569ed4b6daad94a67f6a93db48dedd2c0f
songjie55 <ooo88888@live.cn>
2022-04-19 16:00:59 +0800

    状态模式

---
+++ 状态模式.js
@@ -0,0 +1,39 @@
+//状态模式用来解决过多的If和switch判读的；
+let MarryState = function () {
+    let currentState = {},//用来存储当前需要做的哪些状态
+        states = {//先用一个对象存储所有的状态
+            jump: function () {
+                console.log('jump')
+            },
+            move: function () {
+                console.log('move')
+            },
+            shoot: function () {
+                console.log('shoot')
+            },
+            squat: function () {
+                console.log('squat')
+            }
+        }
+    return {
+        changeState() {
+            let arg = arguments;
+            currentState = {};
+            if (arg.length) {
+                for (let i = 0; i < arg.length; i++) {
+                    currentState[arg[i]] = true;
+                }
+            }
+            return this;
+        },
+        goes() {
+            console.log('触发动作')
+            for (const currentStateKey in currentState) {
+                currentState[currentStateKey] && states[currentStateKey]()
+            }
+            return this;
+        }
+    }
+}
+
+MarryState().changeState('jump', 'shoot').goes().changeState('squat').goes()
