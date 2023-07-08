bba6cf4086ee14d37a45dd7ec7aff9046b1f1475
songjie55 <ooo88888@live.cn>
2023-07-09 00:39:03 +0800

    parseStringToObject

--- 手写promise/parseString.js
+++ 手写promise/parseString.js
@@ -1,33 +1,12 @@
-function convertStringToObject(string) {
-    const stack = [];
-    let currentObject = null;
+let string = "view{head{clothes{}shoes{}}foot{}}";
 
-    for (let i = 0; i < string.length; i++) {
-        const char = string[i];
-
-        if (char === '{') {
-            const newObject = {tag: '', children: []};
-            if (currentObject !== null) {
-                stack.push(currentObject);
-                currentObject.children.push(newObject);
-            }
-            currentObject = newObject;
-        } else if (char === '}') {
-            if (stack.length > 0) {
-                currentObject = stack.pop();
-            }
-        } else if (char.match(/[a-zA-Z]/)) {
-            if (currentObject === null) {
-                currentObject = {tag: char, children: []};
-            } else {
-                currentObject.tag += char;
-            }
-        }
-    }
-
-    return currentObject;
+function parseStringToObject(string) {
+    let x = string.replace(/\{/g, ':{');
+    x = x.replace(/\}/g, '},');
+    x = `{${x}}`
+    x = x.replace(/,}/g, '}');
+    x = x.replace(/([^{}',]+):\{/g, '"$1":{');
+    return JSON.parse(x);
 }
 
-const string = "view{head{clothes{}}foot{}}";
-const result = convertStringToObject(string);
-console.log(result)
+console.log(parseStringToObject(string));
