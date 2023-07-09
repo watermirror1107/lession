bfb2d36f6b9134631c1dfac17da3463306fa66e7
songjie55 <ooo88888@live.cn>
2023-07-09 10:26:12 +0800

    parseStringToObject

--- 手写promise/parseString.js
+++ 手写promise/parseString.js
@@ -2,9 +2,8 @@ let string = "view{head{clothes{}shoes{}}foot{}}";
 
 function parseStringToObject(string) {
     let x = string.replace(/\{/g, ':{');
-    x = x.replace(/\}/g, '},');
+    x = x.replace(/}([^{}]+)/g, '},$1');
     x = `{${x}}`
-    x = x.replace(/,}/g, '}');
     x = x.replace(/([^{}',]+):\{/g, '"$1":{');
     return JSON.parse(x);
 }
